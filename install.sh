1639804513ae6504f5bdd28df2b9c7717d8e66cb
atabekdemurtaza <mychannel21war@gmail.com>
2023-08-08 17:05:30 +0500

    sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# set up
+pip install -r requirements.txt
+
+# run
+python manage.py runserver
+
+# https://www.gnu.org/software/sed/manual/sed.html
+# http://www.asciitable.com/
+# http://linuxconfig.org/add-character-to-the-beginning-of-each-line-using-sed
